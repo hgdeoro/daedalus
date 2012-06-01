c0b4480fa976c498f76c102dae3207574a798488
Horacio G. de Oro <hgdeoro@gmail.com>
2012-06-01 18:06:58 -0300

    Added licence information to sources.

--- src/hgdeoro/lolog/proto/random_log_generator.py
+++ src/hgdeoro/lolog/proto/random_log_generator.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import random
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+##    lolog - Centralized log server
+##    Copyright (C) 2012 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
+##
+##    This file is part of lolog.
+##
+##    lolog is free software; you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 2.
+##
+##    lolog is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License version 2 for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with lolog; see the file LICENSE.txt.
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Copyright (C) 2012 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import random
 
 EXAMPLE_APPS = ['intranet', 'extranet', 'gunicorn', 'nginx-dmz']
 
--- src/hgdeoro/lolog/web_backend/models.py
+++ src/hgdeoro/lolog/web_backend/models.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+##    lolog - Centralized log server
+##    Copyright (C) 2012 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
+##
+##    This file is part of lolog.
+##
+##    lolog is free software; you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 2.
+##
+##    lolog is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License version 2 for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with lolog; see the file LICENSE.txt.
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
