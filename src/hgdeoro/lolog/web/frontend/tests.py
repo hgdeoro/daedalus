32cb226a8fd4b815cce59654a8b130b34e0c2147
Horacio G. de Oro <hgdeoro@gmail.com>
2012-06-02 17:42:19 -0300

    Implemented test methods for frontend.

--- src/hgdeoro/lolog/web/frontend/tests.py
+++ src/hgdeoro/lolog/web/frontend/tests.py
@@ -19,10 +19,40 @@
 ##    along with lolog; see the file LICENSE.txt.
 ##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+import logging
+
 from django.test.testcases import TestCase
+from django.core.urlresolvers import reverse
+
+from hgdeoro.lolog.web.backend.tests import _save_random_messages,\
+    _truncate_all_column_families
+from hgdeoro.lolog.web.frontend import views
+from hgdeoro.lolog.proto.random_log_generator import EXAMPLE_APPS
+
+logger = logging.getLogger(__name__)
 
 
 class WebFrontendTest(TestCase):
 
-    def test(self):
-        pass
+    def test(self, max_count=1000, truncate=True):
+        if truncate:
+            _truncate_all_column_families()
+        _save_random_messages(max_count)
+
+        response = self.client.get(reverse(views.home))
+        self.assertTemplateUsed(response, 'index.html')
+
+        for severity in ('ERROR', 'WARN', 'INFO', 'DEBUG'):
+            logger.info("Testing search by severity: '%s'", severity)
+            response = self.client.get(reverse(views.search_by_severity, args=[severity]))
+            self.assertTemplateUsed(response, 'index.html')
+
+        for app in EXAMPLE_APPS:
+            logger.info("Testing search by app: '%s'", app)
+            response = self.client.get(reverse(views.search_by_application, args=[app]))
+            self.assertTemplateUsed(response, 'index.html')
+
+    def repeated_test(self):
+        logging.basicConfig(level=logging.INFO)
+        while True:
+            self.test(max_count=2000, truncate=False)
