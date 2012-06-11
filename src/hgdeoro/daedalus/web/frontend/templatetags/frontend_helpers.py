9af5b670a0a3df16059ce2fab12ff003fc8dd6cc
Horacio G. de Oro <hgdeoro@gmail.com>
2012-06-11 00:32:32 -0300

    Added custom tempalte tags.

---
+++ src/hgdeoro/daedalus/web/frontend/templatetags/frontend_helpers.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+##    daedalus - Centralized log server
+##    Copyright (C) 2012 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
+##
+##    This file is part of daedalus.
+##
+##    daedalus is free software; you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 2.
+##
+##    daedalus is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License version 2 for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with daedalus; see the file LICENSE.txt.
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+from datetime import datetime
+
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='msg_2_datetime')
+def msg_2_datetime(msg):
+    return datetime.fromtimestamp(float(msg['timestamp']))
+
+
+@register.filter(name='msg_2_id')
+def msg_2_id(msg):
+    return msg['_uuid']
