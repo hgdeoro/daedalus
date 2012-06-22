92382d727261244b9f7f4ad15e318497bd51e25e
Horacio G. de Oro <hgdeoro@gmail.com>
2012-06-22 19:41:09 -0300

    Implemented 'syncdb_cassandra' custom management command.

---
+++ src/hgdeoro/daedalus/web/backend/management/commands/syncdb_cassandra.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+##    daedalus - Centralized log server
+##    Copyright (C) 2012 - Horacio Guillermo de Oro <hgdeoro@gmail.com>
+##
+##    This file is part of daedalus.
+##
+##    daedalus is free software; you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 2.
+##
+##    daedalus is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License version 2 for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with daedalus; see the file LICENSE.txt.
+##-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+from django.core.management.base import BaseCommand
+
+from hgdeoro.daedalus.storage import get_service_cm
+
+
+class Command(BaseCommand):
+    help = 'Creates the keyspace and column families con Cassandra' #@ReservedAssignment
+
+    def handle(self, *args, **options):
+        with get_service_cm() as service:
+            service.create_keyspace_and_cfs()
