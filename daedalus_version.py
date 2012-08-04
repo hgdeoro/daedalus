0f1f6cb0a8848e3e4967d3da9207bb39bf7fe2ce
Horacio G. de Oro <hgdeoro@gmail.com>
2012-08-04 16:13:15 -0300

    Updated version to 0.0.10-dev

--- daedalus_version.py
+++ daedalus_version.py
@@ -1,7 +1,7 @@
 MAJOR_VERSION=0
 MINOR_VERSION=0
-PATCH_VERSION=9
-EXTRA=''
+PATCH_VERSION=10
+EXTRA='-dev'
 
 def get_daedalus_version():
     return "{0}.{1}.{2}{3}".format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION, EXTRA)
